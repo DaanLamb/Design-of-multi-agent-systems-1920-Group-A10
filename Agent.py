c79f23bd8cf1c454cc19e233b73fa46b4c6c5a68
Paul Hofman <paulwhofman@hotmail.com>
2019-09-24 10:31:27 +0200

    Added constants

--- Agent.py
+++ Agent.py
@@ -1,13 +1,16 @@
 import random
+from constants import *
 
 class Agent:
-    def __init__(self, x, y):
+    def __init__(self, x, y):        
         #initialize the agent with its own position and zero points
         self.posx = x
         self.posy = y
         self.points = 0
         self.emotion = 0
         self.strategy = 0
+
+
 
     def updateStatus(self, grid):
         #self.updateEmotion
